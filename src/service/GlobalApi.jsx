526b1d3ba27ae1286c34818323c7cd188e7e33b2
kartikeyakrishna <131996686+kartikeyakrishna@users.noreply.github.com>
2025-08-07 02:37:09 +0530

    Update GlobalApi.jsx

--- src/service/GlobalApi.jsx
+++ src/service/GlobalApi.jsx
@@ -6,7 +6,7 @@
 //       "https://places.googleapis.com/v1/places:searchText",
 //       {
 //         headers: {
-//           Authorization: `Bearer AIzaSyDGfDWOQ0vk1Z_4-Wbh0-J3XgzH917ELIQ`,
+//           Authorization: `Bearer -codehere`,
 //           // Include other headers if necessary
 //         },
 //       }
